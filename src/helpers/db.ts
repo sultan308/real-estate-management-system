b787057ec6761d983a2681fd3641a8fdb7ec1bac
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2023-05-23 20:57:25 +0100

    update to declare db instance outside connection function

--- src/helpers/db.ts
+++ src/helpers/db.ts
@@ -2,10 +2,11 @@ import { MongoClient } from "mongodb";
 
 const DB_URI = process.env.DB_URI || "";
 
-export default async function connectToDatabase(dbName: string = "dev") {
-    const mongoClient = new MongoClient(DB_URI);
+export const MongoDB = new MongoClient(DB_URI);
+
+export async function connectToDatabase(dbName: string = "dev") {
     console.log('Connecting to server');
-    await mongoClient.connect()
+    await MongoDB.connect()
     console.log('Connected successfully to server');
-    const db = mongoClient.db(dbName);
 }
+
