719bd74cd8ac54aa087a800b4750ac9870db11b2
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2023-05-23 07:46:35 +0100

    call db connect helper on start

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 require("dotenv").config();
+import connectToDatabase from "./helpers/db";
 import createServer from "./helpers/server";
 
 const app = createServer();
@@ -7,4 +8,5 @@ const port = process.env.PORT || 3000
 
 app.listen(port, async ()=>{
     console.log(`listening on port ${port}`)
+    await connectToDatabase()
 })
